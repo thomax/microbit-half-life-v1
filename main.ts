16c6f51efeb874acb7e57671a300e35d8208d8c2
Thomas Drevon <thomas@drevon.no>
2021-11-03 14:54:11 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+function decay (led_x: number, led_y: number) {
+    dimmedBrightness = led.pointBrightness(led_x, led_y) / 2
+    led.plotBrightness(led_x, led_y, dimmedBrightness)
+}
 function resetLeds () {
     basic.showLeds(`
         # # # # #
@@ -11,7 +15,7 @@ input.onButtonPressed(Button.A, function () {
     for (let x = 0; x <= 4; x++) {
         for (let y = 0; y <= 4; y++) {
             if (50 > randint(1, 100)) {
-                led.unplot(x, y)
+                decay(x, y)
             }
         }
     }
@@ -19,4 +23,5 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     resetLeds()
 })
+let dimmedBrightness = 0
 resetLeds()
