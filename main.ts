06d3dfc2812791e1fab8774efc0ba3a10bf7ea5d
Thomas Drevon <thomas@drevon.no>
2021-11-03 17:06:55 +0100

    casing fixed

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 function decay (led_x: number, led_y: number) {
-    dimmedBrightness = led.pointBrightness(led_x, led_y) / 2
-    led.plotBrightness(led_x, led_y, dimmedBrightness)
+    dimmed_brightness = led.pointBrightness(led_x, led_y) / 2
+    led.plotBrightness(led_x, led_y, dimmed_brightness)
 }
-function resetLeds () {
+input.onButtonPressed(Button.A, function () {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            if (randint(1, 100) > 50) {
+                decay(x, y)
+            }
+        }
+    }
+})
+function reset_leds () {
     basic.showLeds(`
         # # # # #
         # # # # #
@@ -11,17 +20,8 @@ function resetLeds () {
         # # # # #
         `)
 }
-input.onButtonPressed(Button.A, function () {
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            if (50 > randint(1, 100)) {
-                decay(x, y)
-            }
-        }
-    }
-})
 input.onButtonPressed(Button.B, function () {
-    resetLeds()
+    reset_leds()
 })
-let dimmedBrightness = 0
-resetLeds()
+let dimmed_brightness = 0
+reset_leds()
